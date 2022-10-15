cec19c4e9271a7f1f94e125403fabdf85ed51521
Box <j@box.lol>
2022-10-15 11:30:29 -0400

    🎉 Initial commit!

---
+++ README.md
@@ -0,0 +1,7 @@
+# GET/POST test server
+
+This is a webserver made with [Sanic](https://sanic.dev/) that's used to test GET and POST requests with things like forms and the like.
+
+## Quickstart
+
+To get started, set the `action` method of your `<form>` tag to the URL of the API endpoint (_/api_), and set the method to either **GET** or **POST**. Other than using some spicy JavaScript magic (awesome) or your web browser (lame) to handle the data response, you're all set.
---
+++ server.py
@@ -0,0 +1,42 @@
+from sanic import Sanic, Request, Blueprint
+from sanic.response import json, file
+from sanic_sass import SassManifest
+
+from cors import add_cors_headers
+from options import setup_options
+
+app = Sanic("GET-POST")
+
+# prepare static hosting for website, profile picture, etc.
+app.static("/", "./src/index.html")
+app.static("/examples", "./src/examples.html", name="examples")
+app.static("/profile.jpg", "./src/profile.jpg", name="profile")
+app.static("/css", "./src/css", name="css")
+
+# handle GET requests
+@app.get("/api")
+async def sample_data(req):
+  return json({
+    "first": "John",
+    "last": "Doe",
+    "photo": f"{req.url_for('profile')}"
+  })
+
+# and POST requests, too!
+@app.post("/api")
+async def return_data(req: Request):
+  data = {}
+
+  for item in req.form:
+    data[item] = req.form[item][0]
+
+  return json(data);
+
+app.register_listener(setup_options, "before_server_start")
+app.register_middleware(add_cors_headers, "response")
+
+# run this bad boy
+if __name__ == "__main__":
+  manifest = SassManifest('/css', './src/css', './src/scss', css_type='scss')
+  manifest.compile_webapp(app, register_static=False)
+  app.run(host="0.0.0.0", port=8000)
